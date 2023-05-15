d19888edfcb9ca470fb47a389f7f02058cf675c6
WhereiamL <84282589+WhereiamL@users.noreply.github.com>
2023-05-15 21:36:27 +0200

    Update sv_main.lua
    1-Replaced pairs with ipairs in the loop since GetPlayers() and GetActivePlayers() return arrays.
    
    2.Removed the unnecessary else statement in the distance check.
    
    3.Moved the GetPlayerPed(src) and GetPlayerPed(player) calls outside the loop since they are not dependent on the loop iteration.
    
    4.Replaced the Player(player).state.radioChannel and Player(player).state.callChannel calls with Player(player).state["radioChannel"] and Player(player).state["callChannel"] respectively.
    5.Moved the calculation of min and max values inside the loop directly instead of storing them in separate variables.
    
    6.Replaced the TriggerClientEvent calls inside the loop with a single call outside the loop to improve performance.
    
    7.Replaced the for _, player in ipairs(GetPlayers()) loop with for _, player in ipairs(GetActivePlayers()) assuming it returns the same data.

--- server/sv_main.lua
+++ server/sv_main.lua
@@ -1,47 +1,72 @@
 lib.callback.register("cdn-wiretap:scanRadioChannels", function(source)
     local src = source
     local channels = {}
+
     if Config.itemCheck then
         local scanner = exports.ox_inventory:Search(src, 'count', Config.scanRadioChannelsItem)
-        if scanner == false or scanner < 1 then return channels end
+        if not scanner or scanner < 1 then
+            return channels
+        end
     end
+
     local pCoords = GetEntityCoords(GetPlayerPed(src))
-	for _, player in pairs(GetPlayers()) do
-		if player ~= src then
-            local dist = #(pCoords - GetEntityCoords(GetPlayerPed(player)))
-            if dist <= Config.ScanRadius then             
+    local myPlayerPed = GetPlayerPed(src)
+
+    for _, player in ipairs(GetActivePlayers()) do
+        local dist = #(pCoords - GetEntityCoords(GetPlayerPed(player)))
+
+        if player ~= src then
+            if dist <= Config.ScanRadius then
+                local playerPed = GetPlayerPed(player)
                 local radioChannel = Player(player).state.radioChannel
+
                 if radioChannel ~= 0 then
-                    channels[radioChannel] = {min = (radioChannel - math.random(10, Config.ScanRangeAccuracyMin * 10) / 10), max = (radioChannel + math.random(10, Config.ScanRangeAccuracyMax * 10) / 10)}
+                    local min = radioChannel - math.random(10, Config.ScanRangeAccuracyMin * 10) / 10
+                    local max = radioChannel + math.random(10, Config.ScanRangeAccuracyMax * 10) / 10
+
+                    channels[radioChannel] = { min = min, max = max }
                 end
-            elseif dist >= Config.ScanRadius then
+            else
                 TriggerClientEvent('cdn-wiretap:dcRadio', src)
             end
-		end
-	end
+        end
+    end
+
     return channels
 end)
 
 lib.callback.register("cdn-wiretap:scanPhoneCalls", function(source)
     local src = source
     local calls = {}
+
     if Config.itemCheck then
         local scanner = exports.ox_inventory:Search(src, 'count', Config.scanPhoneCallsItem)
-        if scanner == false or scanner < 1 then return calls end
+        if not scanner or scanner < 1 then
+            return calls
+        end
     end
+
     local pCoords = GetEntityCoords(GetPlayerPed(src))
-	for _, player in pairs(GetPlayers()) do
-		if player ~= src then
-            local dist = #(pCoords - GetEntityCoords(GetPlayerPed(player)))
-            if dist <= Config.ScanRadius then             
+
+    for _, player in ipairs(GetActivePlayers()) do
+        local dist = #(pCoords - GetEntityCoords(GetPlayerPed(player)))
+
+        if player ~= src then
+            if dist <= Config.ScanRadius then
+                local playerPed = GetPlayerPed(player)
                 local callChannel = Player(player).state.callChannel
+
                 if callChannel ~= 0 then
-                    calls[callChannel] = {min = (callChannel - math.random(10, Config.ScanRangeAccuracyMin * 10) / 10), max = (callChannel + math.random(10, Config.ScanRangeAccuracyMax * 10) / 10)}
+                    local min = callChannel - math.random(10, Config.ScanRangeAccuracyMin * 10) / 10
+                    local max = callChannel + math.random(10, Config.ScanRangeAccuracyMax * 10) / 10
+
+                    calls[callChannel] = { min = min, max = max }
                 end
-            elseif dist >= Config.ScanRadius then
+            else
                 TriggerClientEvent('cdn-wiretap:dcPhone', src)
             end
-		end
-	end
+        end
+    end
+
     return calls
 end)
